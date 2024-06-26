574b73a814334c3a8b1cc3fbe2baa946fd8a3b22
Sridhar Sundar <sridharsundar100@gmail.com>
2024-06-26 14:41:53 +0530

    check2

--- build_files.sh
+++ build_files.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 echo "BUILD START"
-cd News-Aggregator
 
 # Install dependencies
-python3.9 -m pip install -r ../requirements.txt
+python3.9 -m pip install -r requirements.txt
+cd News-Aggregator
 
 # Collect static files
 python3.9 manage.py collectstatic --noinput --clear --settings=News-Aggregator.settings
